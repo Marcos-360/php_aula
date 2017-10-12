9120b3366d916cb863af5e741294b357e903f074
Marcos <marcos@efigie.net>
2017-10-11 23:55:58 -0300

    terceiro commit

--- index_c.php
+++ index_c.php
@@ -3,5 +3,16 @@
 $resultado = (10 + 3) / 2 > 5 && 10 + 5 < 20;
 
 var_dump($resultado);
+echo "<br>";
 
+
+$resultado = (10 + 3) / 2 > 5 || 10 + 5 < 3;
+
+var_dump($resultado);
+echo "<br>";
+
+$resultado = (10 + 3) / 2 > 5 && 10 + 5 < 3;
+
+var_dump($resultado);
+echo "<br>";
  ?>
